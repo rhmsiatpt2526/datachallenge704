f9df85427a5773a76d02803b105b43f9495574b5
rhmsiatpt2526 <remi.hamon2022@gmail.com>
2026-06-10 15:02:23 +0200

    ajout randomerasing

--- src/dataset.py
+++ src/dataset.py
@@ -29,6 +29,11 @@ class OcclusionDataset(torch.utils.data.Dataset):
                         mean=[0.485, 0.456, 0.406],
                         std=[0.229, 0.224, 0.225],
                     ),
+                    transforms.RandomErasing(
+                        p=0.10,
+                        scale=(0.02, 0.08),
+                        ratio=(0.3, 3.3),
+                    ),
                 ]
             )
         else:
